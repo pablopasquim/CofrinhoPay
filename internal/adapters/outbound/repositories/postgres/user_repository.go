a128a747807757591815d58529f9a1bd333457e0
Pablo Pasquim <pablo.pasquim7@gmail.com>
2025-10-08 00:47:21 -0300

    ♻️refactor: add GetByUsername and refactor user_repository (repository postgres)

--- internal/adapters/outbound/repositories/postgres/user_repository.go
+++ internal/adapters/outbound/repositories/postgres/user_repository.go
@@ -1,9 +1,9 @@
-// adapters/postgres/repository/user_repository.go
 package repository
 
 import (
 	"errors"
 
+	"github.com/pablopasquim/CofrinhoPay/internal/adapters/outbound/repositories/models"
 	"github.com/pablopasquim/CofrinhoPay/internal/core/domain"
 	"github.com/pablopasquim/CofrinhoPay/internal/core/ports/repositories"
 	"gorm.io/gorm"
@@ -18,52 +18,76 @@ func NewUserRepositoryPostgres(db *gorm.DB) repositories.UserRepository {
 }
 
 func (r *UserRepositoryPostgres) Create(user *domain.User) error {
-	if err := r.db.Create(user).Error; err != nil {
+	userDB := models.UserFromDomain(user)
+	if err := r.db.Create(userDB).Error; err != nil {
 		return err
 	}
+
+	user.ID = userDB.ID
+	user.CreatedAt = userDB.CreatedAt
+	user.UpdatedAt = userDB.UpdatedAt
 	return nil
 }
 
 func (r *UserRepositoryPostgres) GetByID(id uint) (*domain.User, error) {
-	var user domain.User
-	if err := r.db.First(&user, id).Error; err != nil {
+	var userDB models.UserDB
+	if err := r.db.First(&userDB, id).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, nil
 		}
 		return nil, err
 	}
-	return &user, nil
+	return userDB.ToDomain(), nil
+}
+
+func (r *UserRepositoryPostgres) GetByUsername(username string) (*domain.User, error) {
+	var userDB models.UserDB
+	if err := r.db.Where("username = ?", username).First(&userDB).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return userDB.ToDomain(), nil
+}
+
+func (r *UserRepositoryPostgres) GetByEmail(email string) (*domain.User, error) {
+	var userDB models.UserDB
+	if err := r.db.Where("email = ?", email).First(&userDB).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return userDB.ToDomain(), nil
 }
 
 func (r *UserRepositoryPostgres) GetAll() ([]*domain.User, error) {
-	var users []*domain.User
-	if err := r.db.Find(&users).Error; err != nil {
+	var usersDB []models.UserDB
+	if err := r.db.Find(&usersDB).Error; err != nil {
 		return nil, err
+	}
+
+	users := make([]*domain.User, len(usersDB))
+	for i, userDB := range usersDB {
+		users[i] = userDB.ToDomain()
 	}
 	return users, nil
 }
 
 func (r *UserRepositoryPostgres) Update(user *domain.User) error {
-	if err := r.db.Save(user).Error; err != nil {
+	userDB := models.UserFromDomain(user)
+	if err := r.db.Save(userDB).Error; err != nil {
 		return err
 	}
+	// Atualiza os timestamps do domain user
+	user.UpdatedAt = userDB.UpdatedAt
 	return nil
 }
 
 func (r *UserRepositoryPostgres) Delete(id uint) error {
-	if err := r.db.Delete(&domain.User{}, id).Error; err != nil {
+	if err := r.db.Delete(&models.UserDB{}, id).Error; err != nil {
 		return err
 	}
 	return nil
-}
-
-func (r *UserRepositoryPostgres) GetByEmail(email string) (*domain.User, error) {
-	var user domain.User
-	if err := r.db.Where("email = ?", email).First(&user).Error; err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return nil, nil
-		}
-		return nil, err
-	}
-	return &user, nil
 }
